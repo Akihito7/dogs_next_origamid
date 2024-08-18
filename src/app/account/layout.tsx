946689b1277f2c4ca28a4b4f1055db29db8c93fa
akihito7 <akihitopro7@gmail.com>
2024-08-18 16:17:25 -0300

    page account without photos

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -2,8 +2,8 @@ import styles from "./layout.module.css"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
-      <div className={styles.containerMain}>
+    <main className={styles.main}>
+
         <div className={styles.container}>
           <h2 className="title">Minha conta</h2>
 
@@ -23,7 +23,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </div>
 
         </div>
-      </div>
+
       {children}
     </main>
   )
--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import styles from "./index.module.css"
+
 export default function Account() {
   return (
-    <main>
-      <h1>Account</h1>
-    </main>
+    <div className={styles.containerMain}>
+      <p className={styles.p}>Nenhuma foto encontrada</p>
+
+      <Link href={"/account/post"} >
+        <button
+          className={styles.button}
+        >
+          Postar foto
+        </button>
+      </Link>
+    </div>
   );
 }
